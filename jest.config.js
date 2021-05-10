9f1330c490db2b0a655e0fc06a72ea2667bcbe3e
Elliott Regan <elliott.regan@hey.com>
2021-05-10 09:10:28 -0600

    fix tests

--- jest.config.js
+++ jest.config.js
@@ -2,12 +2,6 @@ module.exports = {
   testEnvironment: 'jsdom',
   transform: {
     '^.+\\.vue$': 'vue-jest',
-  },
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(js|mjs)$',
-  moduleFileExtensions: ['vue', 'js', 'mjs'],
-  globals: {
-    'vue-jest': {
-      babelConfig: true,
-    },
+    '^.+\\js$': 'babel-jest'
   },
 }
--- tests/components/HelloWorld.spec.js
+++ tests/components/HelloWorld.spec.js
@@ -6,6 +6,7 @@ import HelloWorld from '../../src/components/HelloWorld.vue'
 describe('HelloWorld.vue', () => {
   it('renders a greeting', () => {
     const wrapper = mount(HelloWorld, {
+      props: { count: 5 },
       data() {
         return {
           message: 'world'
@@ -15,18 +16,10 @@ describe('HelloWorld.vue', () => {
   
     expect(wrapper.html()).toContain('Hello world')
   })
-  it('renders a count', () => {
-    const wrapper = mount(HelloWorld, {
-      props: {
-        count: 5
-      },
-      data() {
-        return {
-          message: 'world'
-        }
-      }
-    })
   
-    expect(wrapper.html()).toContain('Count: 5')
+  it('returns a single prop applied to a component and renders', () => {
+    const wrapper = mount(HelloWorld, { props: { count: 5 } })
+    expect(wrapper.props('count')).toEqual(5)
   })
+
 })
